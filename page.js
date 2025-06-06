a918999a3a3b6a6de627561302539385ab444bbc
DerJust1n <justyn.paul.anke@gmail.com>
2025-06-06 21:58:34 +0200

    Little Code style changes + new animated background

--- page.js
+++ page.js
@@ -6,3 +6,20 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('open');
   document.body.classList.toggle('menu-open');
 });
+
+
+VANTA.FOG({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0xff0e00,
+  midtoneColor: 0xff8600,
+  lowlightColor: 0x0,
+  baseColor: 0x0,
+  blurFactor: 0.40,
+  speed: 0.10,
+  zoom: 0.10
+});
